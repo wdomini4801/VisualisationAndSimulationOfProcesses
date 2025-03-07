c6df160b5c77e1b0e73175a058569c5380408ff0
Wojciech Dominiak <wojciechdominiak80@gmail.com>
2025-03-07 20:28:29 +0100

    add comments

--- Lista1/graphs.py
+++ Lista1/graphs.py
@@ -1,16 +1,41 @@
+# zaimportuj bibliotekę ’networkx’ do rysowania grafów
+# nazwij ją ’nx’
+# dalej można z niej korzystać w kodzie pod nazwą ’nx’
 import networkx as nx
+
+# zaimportuj bibliotekę ’pyplot’ z pakietu ’matplotlib’
+# nazwij ją ’plt’
+# dalej można z niej korzystać w kodzie pod nazwą ’plt’
 import matplotlib.pyplot as plt
 
+# utwórz obiekt reprezentujący graf ’nx.Graph()’
+# przypisz go do zmiennej ’G’
 G = nx.Graph()
+
+# wykorzystaj funkcję ’add_edge’ obiektu/grafu ’G’
+# funkcja dodaje do grafu krawędź między dwoma wierzchołkami
+# nazwy wierzchołków podane są w argumentach funkcji
 G.add_edge('A', 'B')
 G.add_edge('B', 'D')
 G.add_edge('A', 'C')
 G.add_edge('C', 'D')
 
+# wybierz typ układu wierzchołków ’spring_layout’
+# przypisz go do zmiennej ’pos’
 pos = nx.spring_layout(G)
 
+# wyświetl wierzchołki
+# wierzchołki są w pozycjach zadanych przez ’pos’
+# wierzchołki mają rozmiar ’500’
 nx.draw_networkx_nodes(G, pos, node_size=500)
+
+# wyświetl etykiety wierzchołków
+# etykiety są w pozycjach zadanych przez ’pos’
 nx.draw_networkx_labels(G, pos)
+
+# wyświetl krawędzie grafu
+# wierzchołki są w pozycjach zadanych przez ’pos’
 nx.draw_networkx_edges(G, pos)
 
+# wyświetl graf
 plt.show()
--- Lista1/main.py
+++ Lista1/main.py
@@ -1,5 +1,15 @@
+# zaimportuj bibliotekę ’pyplot’ z pakietu ’matplotlib’
+# nazwij ją ’plt’
+# dalej można z niej korzystać w kodzie pod nazwą ’plt’
 import matplotlib.pyplot as plt
 
+# utwórz wykres
+# funkcja ’plt.subplots’ zwraca dwa argumenty
+# pierwszy ignorujemy, drugi zapisujemy do zmiennej ’axes’
 _, axes = plt.subplots()
+
+# tworzymy wykres z punktów (0, 0), (1, 2), (2, 4), itd.
 axes.plot([0, 1, 2, 3, 4, 5, 6], [0, 2, 4, 6, 4, 2, 0])
+
+# wyświetlamy wykres
 plt.show()
